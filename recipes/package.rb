832ce774ebff806a2eec218d1e4b1f3f790cec21
Cyprien DIOT <cyprien.diot@pmsipilot.com>
2015-05-21 14:20:09 +0200

    fixing issues

--- recipes/package.rb
+++ recipes/package.rb
@@ -1,13 +1,11 @@
 if node["platform"] == "debian"
   include_recipe 'debian-backports'
-
-  if not node['backports']['install'].empty?
-    apt_package node['backports']['install'] do
+  node['packages'].each do |pkg|
+    apt_package pkg do
       default_release "#{node['lsb']['codename']}-backports"
       action :install
     end
   end
-
 else
   log 'Debian backports packages install can only be set up on debian' do
     level :warn
